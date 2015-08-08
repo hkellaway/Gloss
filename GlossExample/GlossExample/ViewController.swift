18eb8b61a31d31d8a621b89717e37b6bf44141dc
Harlan Kellaway <harlan@prolificinteractive.com>
2015-08-08 18:02:38 -0400

    Created Xcode project

---
+++ GlossExample/GlossExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GlossExample
+//
+//  Created by Harlan Kellaway on 8/8/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
