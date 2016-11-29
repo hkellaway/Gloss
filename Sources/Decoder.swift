d864079d03d8dedab91365366d40f578cab358f3
Edinson Mejia <edmejia@me.com>
2016-11-30 10:36:35 +1100

    Makes Decoder.decode(enumArrayForKey:) non exclusive of the Enum type

--- Sources/Decoder.swift
+++ Sources/Decoder.swift
@@ -259,7 +259,7 @@ public struct Decoder {
                 
                 for rawValue in rawValues {
                     guard let enumValue = T(rawValue: rawValue) else {
-                        return nil
+                        continue
                     }
                     
                     enumValues.append(enumValue)
