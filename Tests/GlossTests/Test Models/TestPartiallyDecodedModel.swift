215d1267a77b32ef892f98206a76cba74d949eb7
Edinson Mejia <edmejia@me.com>
2016-12-17 19:53:42 +1100

    Updated credits

--- Tests/GlossTests/Test Models/TestPartiallyDecodedModel.swift
+++ Tests/GlossTests/Test Models/TestPartiallyDecodedModel.swift
@@ -2,7 +2,7 @@
 //  TestNestedModel.swift
 //  GlossExample
 //
-// Copyright (c) 2015 Harlan Kellaway
+// Copyright (c) 2016 Edison Mejia
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
