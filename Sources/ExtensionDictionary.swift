230525aaaef14288e262cbf343f63a7f6c2228af
Deepak Badiger <deepak.badiger@synechron.com>
2018-04-19 11:35:37 -0400

    fixed compactMap compile issue

--- Sources/ExtensionDictionary.swift
+++ Sources/ExtensionDictionary.swift
@@ -79,7 +79,7 @@ extension Dictionary {
      - returns: New dictionary of transformed values.
      */
     internal func flatMap<KeyPrime : Hashable, ValuePrime>(_ transform: (Key, Value) throws -> (KeyPrime, ValuePrime)?) rethrows -> [KeyPrime : ValuePrime] {
-        return Dictionary<KeyPrime,ValuePrime>(elements: try flatMap({ (key, value) in
+        return Dictionary<KeyPrime,ValuePrime>(elements: try compactMap({ (key, value) in
             return try transform(key, value)
         }))
     }
