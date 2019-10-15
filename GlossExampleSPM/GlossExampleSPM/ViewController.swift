0a4092314bcb8dc56f12bb80e666ca20eb04d1fd
Harlan Kellaway <hkellaway@users.noreply.github.com>
2019-10-15 11:18:14 -0400

    Create SwiftPM Example project

---
+++ GlossExampleSPM/GlossExampleSPM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlossExampleSPM
+//
+//  Created by Harlan Kellaway on 10/15/19.
+//  Copyright © 2019 Harlan Kellaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
